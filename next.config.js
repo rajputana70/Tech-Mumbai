765cec551c74de466aee378242c92655af2f95d4
rajputana70 <nagesh@trainingbasket.co>
2025-04-01 14:18:20 +0530

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,52 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Added output configuration
-  distDir: 'out', // Added distDir configuration
-  reactStrictMode: false, // Added reactStrictMode configuration
+  output: 'export',
+  distDir: 'out',
   images: {
     unoptimized: true,
     domains: [
       "source.unsplash.com",
       "images.unsplash.com",
-      "ext.same-assets.com",
-      "ugc.same-assets.com",
     ],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "source.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ext.same-assets.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ugc.same-assets.com",
-        pathname: "/**",
-      },
-    ],
-  },
-  webpack: (config) => { // Added webpack configuration
-    config.module.rules.push({
-      test: /\.(mp4|webm|ogg)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next/static/media/',
-          outputPath: 'static/media/',
-          name: '[name].[hash].[ext]',
-        },
-      },
-    });
-    return config;
   },
 };
 
